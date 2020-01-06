4788828f93110c38c8897ce06251b58c02be50d0
Hugh Hunter <hugh@bollandbranch.com>
2020-01-06 16:41:13 -0500

    Remove un-used categories … just want “Do Not Sell” custom category

--- src/consent-manager/categories.ts
+++ src/consent-manager/categories.ts
@@ -1,27 +1,5 @@
-export const MARKETING_AND_ANALYTICS_CATEGORIES = [
-  'A/B Testing',
-  'Analytics',
-  'Attribution',
-  'Email',
-  'Enrichment',
-  'Heatmaps & Recordings',
-  'Raw Data',
-  'Realtime Dashboards',
-  'Referrals',
-  'Surveys',
-  'Video'
-]
+export const MARKETING_AND_ANALYTICS_CATEGORIES = []
 
-export const ADVERTISING_CATEGORIES = ['Advertising', 'Tag Managers']
+export const ADVERTISING_CATEGORIES = []
 
-export const FUNCTIONAL_CATEGORIES = [
-  'CRM',
-  'Customer Success',
-  'Deep Linking',
-  'Helpdesk',
-  'Livechat',
-  'Performance Monitoring',
-  'Personalization',
-  'SMS & Push Notifications',
-  'Security & Fraud'
-]
+export const FUNCTIONAL_CATEGORIES = []
